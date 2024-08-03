8514336d0c695b74916af754056be6695520bba8
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-08-02 23:36:11 -0400

    add data
    I added data in my tables to test out.

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,11 +1,12 @@
 INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('3 Fauster', 'Mother and Baby', 3);
 INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('3 West', 'Intensive Care', 3);
 INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('3 Neuman', 'Labor and Delivery', 3);
-INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('4 Fauster', 'Cardiology', 4);
-INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('5', 'Pediatric', 5);
-INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('6', '', 6);
-INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('7', '', 7);
-INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('8', '', 8);
+INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('4 Neuman', 'Cardiology', 4);
+INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('4 South', 'CICU', 4);
+INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('4 Fauster', 'Peds', 4);
+INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('5 Fauster', 'Pediatric', 5);
+INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('5 Neuman', 'PICU', 5);
+INSERT INTO department (department_floor, department_name, department_floor_number) VALUES ('5 South', 'Surgery Recovery', 5);
 
 
 
@@ -15,4 +16,15 @@ INSERT INTO housekeeper (housekeeper_first_name, housekeeper_last_name, housekee
 INSERT INTO housekeeper (housekeeper_first_name, housekeeper_last_name, housekeeper_pager) VALUES ('Edward', 'Rose', '5611');
 INSERT INTO housekeeper (housekeeper_first_name, housekeeper_last_name, housekeeper_pager) VALUES ('Bluey', 'Heeler', '0589');
 
-
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (1, 1);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (2, 1);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (3, 1);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (4, 2);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (5, 2);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (6, 2);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (7, 3);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (8, 3);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (9, 3);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (7, 4);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (3, 4);
+INSERT INTO department_housekeeper (department_id, housekeeper_id) VALUES (5, 4);
